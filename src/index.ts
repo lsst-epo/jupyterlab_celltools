78e8d51a2409a38c26635edce987d59aa6e14763
Abbey Yacoe <ayacoe@lsst.org>
2018-05-02 16:49:05 -0700

    extension run on page load and refresh by specific notebooks

--- src/index.ts
+++ src/index.ts
@@ -12,23 +12,29 @@ import {
   ICommandPalette
 } from '@jupyterlab/apputils';
 
+import {
+  INotebookTracker 
+} from '@jupyterlab/notebook';
+
+
+const NOTEBOOK = "foundational.ipynb";
+
 /**
  * Initialization data for the jupyterlab_hidecode extension.
  */
 const extension: JupyterLabPlugin<void> = {
   id: 'jupyterlab_hidecode',
   autoStart: true,
-  requires: [ICommandPalette],
-  activate: (app: JupyterLab, palette: ICommandPalette) => {
+  requires: [ICommandPalette, INotebookTracker],
+  activate: (app: JupyterLab, palette: ICommandPalette, tracker: INotebookTracker) => {
   console.log('JupyterLab extension Hide The Code is activated!');
   console.log('ICommandPalette:', palette);
-      
+        
     // Create a single widget
     let widget: Widget = new Widget();
     widget.id = 'hidecode-jupyterlab';
     widget.title.label = 'tab title';
     widget.title.closable = true;
-      
 
   
     // Add an application command
@@ -37,12 +43,21 @@ const extension: JupyterLabPlugin<void> = {
         label: 'Hide The Code',
         execute: () => {
           console.log('Success!');
-          app.commands.execute('notebook:hide-all-cell-code');
-          
+          app.commands.execute('notebook:hide-all-cell-code');          
         }
       });
       // Add the command to the palette.
       palette.addItem({command, category: 'Extensions'});
+
+
+      // Add an event listener that grabs notebooks by name(for now) and hides the code.
+      document.addEventListener("click", function(event) {
+        var target = <HTMLElement> event.target;
+          if (String(target.innerHTML) || String(tracker.currentWidget.title.label) == NOTEBOOK) {
+            setTimeout(function() { app.commands.execute('hidecode:hidecode')}, 200);
+          }
+        });
+      setTimeout(function() { document.body.click();}, 100);
     }  
 };
 
