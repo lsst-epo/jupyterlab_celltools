df7b8826b40cfc87db393079bcef4ca9a706e09e
Abbey Yacoe <ayacoe@lsst.org>
2018-05-04 14:30:31 -0700

    working hide the code on pageload

--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,12 @@ import {
 import '../style/index.css';
 
 import {
-  Widget
-} from '@phosphor/widgets';
-
-import {
   ICommandPalette
 } from '@jupyterlab/apputils';
 
 import {
-  INotebookTracker 
+  INotebookTracker
 } from '@jupyterlab/notebook';
-
-
-const NOTEBOOK = "foundational.ipynb";
 
 /**
  * Initialization data for the jupyterlab_hidecode extension.
@@ -28,37 +21,27 @@ const extension: JupyterLabPlugin<void> = {
   requires: [ICommandPalette, INotebookTracker],
   activate: (app: JupyterLab, palette: ICommandPalette, tracker: INotebookTracker) => {
   console.log('JupyterLab extension Hide The Code is activated!');
-  console.log('ICommandPalette:', palette);
-        
-    // Create a single widget
-    let widget: Widget = new Widget();
-    widget.id = 'hidecode-jupyterlab';
-    widget.title.label = 'tab title';
-    widget.title.closable = true;
 
-  
-    // Add an application command
-    const command: string = 'hidecode:hidecode';
-    app.commands.addCommand(command, {
-        label: 'Hide The Code',
-        execute: () => {
-          console.log('Success!');
-          app.commands.execute('notebook:hide-all-cell-code');          
-        }
-      });
-      // Add the command to the palette.
-      palette.addItem({command, category: 'Extensions'});
+  function executeActions() {
+    // Active cell Number.
+    tracker.currentWidget.notebook.activeCellIndex = 0;
+    
+    for (var i = 0; i < tracker.currentWidget.model.cells.length -1; i++) {
+      // checks metadata and executes 
+        if (tracker.currentWidget.notebook.activeCell.model.metadata.get("hideCode") == "true") {
+          app.commands.execute('notebook:hide-cell-code');   
+        } 
+        tracker.currentWidget.notebook.activeCellIndex++;
+      }     
+    }
 
 
-      // Add an event listener that grabs notebooks by name(for now) and hides the code.
-      document.addEventListener("click", function(event) {
-        var target = <HTMLElement> event.target;
-          if (String(target.innerHTML) || String(tracker.currentWidget.title.label) == NOTEBOOK) {
-            setTimeout(function() { app.commands.execute('hidecode:hidecode')}, 200);
-          }
-        });
-      setTimeout(function() { document.body.click();}, 100);
-    }  
-};
+  tracker.currentChanged.connect(() => { 
+     setTimeout(executeActions, 100);
+  });
+  setTimeout(executeActions, 1000); 
+  }
+
+}
 
 export default extension;
