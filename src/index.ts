e1522e19e7f5eab3488451ecb47557d7af77c7f7
Christine Banek <cbanek@lsst.org>
2018-06-12 12:28:00 -0700

    [EPO-793] Race condition between running code and kernel being ready.
    When loading the notebook, the python kernel seems to be one of the
    last things to get started.  If the kernel isn't ready, and you try
    to run the code, it won't run, but won't error either.  Add this
    case to the section of code that will retry.

--- src/index.ts
+++ src/index.ts
@@ -25,9 +25,9 @@ const extension: JupyterLabPlugin<void> = {
 
 
    function executeActions() {
-    // If the notebook isn't loaded by the time we get run,
+    // If the notebook or kernel isn't loaded by the time we get run,
     // reschedule us to run again.
-    if (tracker.currentWidget == null) {
+    if (tracker.currentWidget == null || !tracker.currentWidget.session.isReady) {
         console.log('Not ready to hide the code yet.');
         setTimeout(executeActions, 100);
         return;
